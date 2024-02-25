5b427fcaed6dec6b1f67b3ec64329ac2573a1f9c
Hazim50 <74001552+Hazim50@users.noreply.github.com>
2024-02-25 12:14:17 +0300

    Create image_generator.py

--- image_generator.py
+++ image_generator.py
@@ -2,68 +2,63 @@ import cv2
 import numpy as np
 import os
 
-class ColorGenerator:
-    def __init__(self):
-        self.color_dict = {
-            "white": (255, 255, 255),
-            "black": (0, 0, 0),
-            "gray": (128, 128, 128),
-            "red": (0, 0, 255),
-            "blue": (255, 0, 0),
-            "green": (0, 255, 0),
-            "yellow": (0, 255, 255),
-            "purple": (173, 13, 106),
-            "brown": (0, 75, 150),
-            "orange": (39, 127, 255)
-        }
+COLOR_DICT = {
+    "white": (255, 255, 255),
+    "black": (0, 0, 0),
+    "gray": (128, 128, 128),
+    "red": (0, 0, 255),
+    "blue": (255, 0, 0),
+    "green": (0, 255, 0),
+    "yellow": (0, 255, 255),
+    "purple": (173, 13, 106),
+    "brown": (0, 75, 150),
+    "orange": (39, 127, 255)
+}
 
-    def get_color(self, color_name):
-        return self.color_dict.get(color_name)
-
-class ImageGenerator:
-    def __init__(self, color_generator):
-        self.color_generator = color_generator
-
-    def generate_image(self, background_color, text_color, material, location):
-        backgr = np.zeros(shape=[400, 400, 3], dtype=np.uint8)
-        background_frame = np.copy(backgr)
-        text_color_value = self.color_generator.get_color(text_color)
-
-        if material == "I":
-            text_location = (150, 300)
-        else:
-            text_location = (92, 300)
-
-        cv2.putText(background_frame, material, text_location, fontFace, fontScale, text_color_value, thickness, lineType)
-        image_name = f"{location}{background_color}-{text_color}-{material}.png"
-        cv2.imwrite(image_name, background_frame)
-        print(f"{image_name} file has created")
+MATERIAL_LIST = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+                 "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 def create_folder_if_not_exists(folder_path):
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
         print(f"{folder_path} folder created.")
 
-if __name__ == "__main__":
-    fontFace = 2
-    fontScale = 11
-    thickness = 11
-    lineType = cv2.FONT_HERSHEY_SIMPLEX
-    location = "C:\\Users\\Hazim\\Desktop\\folder\\Materials\\" # WARNING Don't write desktop location
-                                                                # Also, last 2 slash is important
 
-    color_generator = ColorGenerator()
-    image_generator = ImageGenerator(color_generator)
+def create_background(color, size=(400, 400)):
+    return np.zeros(shape=[size[0], size[1], 3], dtype=np.uint8) + color
 
-    create_folder_if_not_exists(location)
 
-    material_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+def place_text(img, text, position, color):
+    if text == "I":
+        cv2.putText(img, text, position, 2, 11, color, 11, cv2.LINE_AA)
+    else:
+        cv2.putText(img, text, position, 2, 11, color, 11, cv2.LINE_AA)
+    return img
 
-    for background_color in color_generator.color_dict:
-        for text_color in color_generator.color_dict:
-            if background_color == text_color:
+
+def save_images(background_dict, color_dict, material_list, save_location):
+    for bg_color, bg_image in background_dict.items():
+        for text_color, text_color_value in color_dict.items():
+            if bg_color == text_color:
                 continue
             for material in material_list:
-                image_generator.generate_image(background_color, text_color, material, location)
+                pos=(92, 300)
+                if material == "I":
+                    pos=(150, 300)
+                img = place_text(bg_image.copy(), material, pos, text_color_value)
+                img_path = f"{save_location}{bg_color}-{text_color}-{material}.png"
+                print(f"{img_path} has created")
+                cv2.imwrite(img_path, img)
 
-    cv2.destroyAllWindows()
+def main():
+    background_dict = {}
+    for color_name, color_value in COLOR_DICT.items():
+        background_dict[color_name] = create_background(color_value)
+
+    save_location = "C:\\Users\\Hazim\\Desktop\\Materials\\"
+    create_folder_if_not_exists(save_location)
+    save_images(background_dict, COLOR_DICT, MATERIAL_LIST, save_location)
+
+
+if __name__ == "__main__":
+    main()
